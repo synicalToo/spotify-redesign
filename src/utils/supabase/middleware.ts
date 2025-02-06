9d5b7685ef74c091369f1cce4caed0344aa868d1
synical <87197716+synicalToo@users.noreply.github.com>
2025-02-07 08:47:27 +0900

    package.json: -updated dependencies -add zod for server validation
    src:
    -added sign-in page placeholder
    -added sign-up page placeholder
    -added forgot-password page placeholder
    
    -updated supabase middleware for auto redirecting to sign-in page

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -29,10 +29,10 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!user && !request.nextUrl.pathname.startsWith("/login") && !request.nextUrl.pathname.startsWith("/auth")) {
+  if (!user && !request.nextUrl.pathname.startsWith("/sign-in") && !request.nextUrl.pathname.startsWith("/sign-up") && !request.nextUrl.pathname.startsWith("/forgot-password") && !request.nextUrl.pathname.startsWith("/(auth)")) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
-    url.pathname = "/login";
+    url.pathname = "/sign-in";
     return NextResponse.redirect(url);
   }
 
